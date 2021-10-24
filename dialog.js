cb43ceefa3c2dfe91e15cc25f144dd9648b3ad3d
abdussamadbello <abdussamadbello4real@hotmail.co.uk>
2021-10-24 20:41:10 +0000

    Initial commit

---
+++ dialog.js
@@ -0,0 +1,15 @@
+export default class DialogBox {
+
+  constructor(message,confirm,cancel){
+  this.message=message
+  this.confirm=confirm
+  this.cancel=cancel
+  }
+
+  notification(button){
+   let clickedButton = (button==this.confirm)?this.confirm:this.cancel
+    return `you just clicked ${clickedButton}`
+  }
+
+  
+}
---
+++ script.js
@@ -0,0 +1,6 @@
+import dialog from './dialog.js'
+
+let mbox = new dialog('Are you sure you want to continue?’'
+, 'yes','no')
+
+mbox.notification('yes')
