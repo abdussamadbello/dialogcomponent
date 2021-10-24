b2c6feb14aad4388fdfff3a4ab41f989b1e35f94
abdussamadbello <abdussamadbello4real@hotmail.co.uk>
2021-10-24 22:40:59 +0000

    dynamic popup

--- script.js
+++ script.js
@@ -1,32 +1,80 @@
 import dialog from './dialog.js'
 
-let mbox = new dialog('Are you sure you want to continue?’'
-  , 'yes', 'no')
+//initialize Box
+let newbox = new dialog('Are you sure you want to continue?’', 'yes', 'no')
 
-mbox.notification('yes')
 
 
 // Get the modal
-let modal = document.getElementById("dialogbox");
+let dialogBox = document.getElementById("dialogbox");
 
 // Get the button that opens the modal
-let btn = document.getElementById("dialogButton");
+let dialogButton = document.getElementById("dialogButton");
 
 let span = document.getElementById("close");
 
+//set Box with varibles
+function setDialogBox(dialogBox) {
+
+  document.getElementById("dialogMessage").innerHTML = dialogBox.message;
+
+  document.getElementById("confirmBtn").innerText = dialogBox.confirm;
+
+  document.getElementById("cancelBtn").innerText = dialogBox.cancel;
+
+}
+
+
+
+//Clear Box with varibles
+function clearDialogBox() {
+
+  document.getElementById("dialogMessage").innerHTML = ""
+
+  document.getElementById("confirmBtn").innerText = ""
+
+  document.getElementById("cancelBtn").innerText = "";
+
+  dialogBox.style.display = "none";
+
+}
+
+
+
+//Initialize box with dialog Class
+setDialogBox(newbox)
+
+
 // When the user clicks the button, open the modal 
-btn.onclick = function () {
-  modal.style.display = "block";
+dialogButton.onclick = function () {
+  document.getElementById("notficationContent")
+    .innerHTML =''
+  dialogBox.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-  modal.style.display = "none";
+  dialogBox.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+//set button variables
+let confirmBtn = document.getElementById("confirmBtn")
+
+let cancelBtn = document.getElementById("cancelBtn")
+
+// set Onclick listeners
+confirmBtn.onclick = function () { notify(newbox, confirmBtn.innerText) };
+
+cancelBtn.onclick = function () { notify(newbox, cancelBtn.innerText) };
+
+
+//append message
+
+function notify(dialog, button) {
+
+  document.getElementById("notficationContent")
+    .innerHTML = `<p> ${dialog.notification(button)}</p>`
+
+    dialogBox.style.display = "none";
 }
+
